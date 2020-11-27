d1810925fa84cd7cb66a10a610e15f52caa83ba4
Laverone <73112392+Laverone@users.noreply.github.com>
2020-11-27 21:51:35 +0100

    First Commit

---
+++ src/test/java/com/java/poo/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.poo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
